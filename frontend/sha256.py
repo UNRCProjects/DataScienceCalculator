f95ff4425c081db6aed709f86e757756c92dd003
M Evangelista <miguel.eva.alv@gmail.com>
2025-11-09 22:08:56 -0600

    Delete frontend/sha256.py

--- frontend/sha256.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def hash_text(text: str) -> str:
-    """
-    Calcula el hash SHA-256 de un texto dado.
-    """
-    sha256_hash = hashlib.sha256(text.encode('utf-8')).hexdigest()
-    return sha256_hash
